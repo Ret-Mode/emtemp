b47b69ee15f1f0d6dea644d32483214065c42cba
Ret-Mode <ernest.harcej@wp.pl>
2025-12-13 21:51:33 +0100

    Added Emscripten

---
+++ embuild.sh
@@ -0,0 +1,2 @@
+emcmake cmake -B_ebuild -H.
+cmake --build _ebuild
--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#include <SDL2/SDL.h>
+bool one_iter(double time, void* userData) {
+  return true;
+}
 
-int main(){
-  std::cout<<"_";
-  return 0;
+int main() {
+#ifdef __EMSCRIPTEN__
+  // Receives a function to call and some user data to provide it.
+  emscripten_request_animation_frame_loop(one_iter, 0);
+#else
+  while (1) {
+    one_iter();
+    // Delay to keep frame rate constant (using SDL).
+    SDL_Delay(1);
+  }
+#endif
+return 0;
 }
